bbef9864dbf1d7b2afd5da37566d81774604aa5f
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2024-01-24 22:25:23 +0800

    stack plot

---
+++ p4/main.py
@@ -0,0 +1,13 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+plt.style.use('ggplot')
+
+minutes = list(np.arange(1,10))
+
+player1 = [1,2,3,3,4,4,4,4,5]
+player2 = [1,1,1,1,2,2,2,3,4]
+player3 = [1,1,1,2,2,2,3,3,3]
+
+
+
