b021dee79d9e5f9fa00c10606611eb1aef3958a5
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2024-01-25 10:35:50 +0800

    stack plots

--- p4/main.py
+++ p4/main.py
@@ -9,5 +9,18 @@ player1 = [1,2,3,3,4,4,4,4,5]
 player2 = [1,1,1,1,2,2,2,3,4]
 player3 = [1,1,1,2,2,2,3,3,3]
 
+# plt.pie([1,1,1], labels=['p1','p2','p3'])
+labels = ['p1','p2','p3']
+colors = ['#6EFF88', '#6EB2FF', '#B982FF', '#AFAFAF']
+plt.stackplot(minutes, player1, player2, player3, labels=labels, colors=colors)
+
+plt.title('stack plot')
+plt.tight_layout()
+plt.legend(loc='upper left')
+plt.savefig('model')
+plt.show()
+
+
+
 
 
