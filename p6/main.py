6af6c02a9a9b9699d36b1973279cf966fdaffa88
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2024-01-25 19:26:16 +0800

    filling area(line plot)

--- p6/main.py
+++ p6/main.py
@@ -4,25 +4,27 @@ from matplotlib import pyplot as plt
 plt.style.use('fivethirtyeight')
 # plt.style.use('ggplot')
 
-data = pd.read_csv('data.csv')
+data = pd.read_csv('p6/data.csv')
 ids = data['Responder_id']
 ages = data['Age']
 
 # ages = [18, 19, 21, 25, 26, 26, 30, 32, 38, 45, 55]
 bins = [10,20,30,40,50,60,70,80,90,100]
 
-plt.hist(ages, bins=bins, edgecolor='black')
+plt.hist(ages, bins=bins, edgecolor='black', log=True)
 
 
-# median_age = 29
-# color = '#fc4f30'
+median_age = 29
+color = '#fc4f30'
 
-# plt.legend()
+plt.axvline(median_age, color=color, label='age median', linewidth=2)
+
+plt.legend()
 
 plt.title('Ages of Respondents')
 plt.xlabel('Ages')
 plt.ylabel('Total Respondents')
 
 plt.tight_layout()
-plt.savefig('model')
+plt.savefig('model1.png')
 plt.show()
