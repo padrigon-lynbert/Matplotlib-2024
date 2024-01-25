79e89f2c330504017da13ff6eb46b16dc6f700d9
Padrigon, Lynbert <123610073+Skilt@users.noreply.github.com>
2024-01-25 11:53:48 +0800

    histograms

---
+++ p6/main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+plt.style.use('fivethirtyeight')
+# plt.style.use('ggplot')
+
+data = pd.read_csv('data.csv')
+ids = data['Responder_id']
+ages = data['Age']
+
+# ages = [18, 19, 21, 25, 26, 26, 30, 32, 38, 45, 55]
+bins = [10,20,30,40,50,60,70,80,90,100]
+
+plt.hist(ages, bins=bins, edgecolor='black')
+
+
+# median_age = 29
+# color = '#fc4f30'
+
+# plt.legend()
+
+plt.title('Ages of Respondents')
+plt.xlabel('Ages')
+plt.ylabel('Total Respondents')
+
+plt.tight_layout()
+plt.savefig('model')
+plt.show()
